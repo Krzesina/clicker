1af5e279fa78736624381f558fa84afee52df4df
Terrarian70 <46934544+Terrarian70@users.noreply.github.com>
2019-10-24 12:57:51 +0200

    Update config.php

--- beecker/user system/config.php
+++ beecker/user system/config.php
@@ -8,7 +8,7 @@ $cfg['db_name'] = 'users';
 
 
 
-$conn = @mysqli_connect ($cfg['db_server'], $cfg['db_user'], $cfg['db_pass'], $cfg['db_name']);
+$conn = mysqli_connect ($cfg['db_server'], $cfg['db_user'], $cfg['db_pass'], $cfg['db_name']);
 
 if (!$conn) {
     die ('<p class="error">Nie udało się połączyć z bazą danych.</p>');
