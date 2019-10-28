f77b42f33a339679bc5b690047edb21398bc205e
Nooobert <42031450+Nooobert@users.noreply.github.com>
2019-10-28 17:45:34 +0100

    Add files via upload

--- beecker/js/main.js
+++ beecker/js/main.js
@@ -1,7 +1,8 @@
+// pozdro dla was
 let money = 0
 
 let pszczoly = [
-    { nazwa: "Pszczola", cena: 10, coIle: 2000, ile: 0, interval: null },
+    { nazwa: "Pszczola", cena: 10, coIle: 2000, ile: 0, interval: null, klasa: 'xd1'},
     { nazwa: "Super Pszczola", cena: 100, coIle: 1000, ile: 0, interval: null },
     { nazwa: "SSJPszczola", cena: 300, coIle: 800, ile: 0, interval: null },
     { nazwa: "Magnum Pszczola", cena: 500, coIle: 500, ile: 0, interval: null },
@@ -23,33 +24,33 @@ let upgrade = [
 let clickCost = 5;
 let clickValue = 1;
 
-let currentvalue = document.querySelector('.currentvalue');
+let currentmoney = document.querySelector('.currentmoney');
 function wyswietl() {
     let przechowaj = money;
     if (przechowaj >= 1e3 && przechowaj < 1e6) {
-        currentvalue.textContent = (przechowaj / 1e3).toFixed(2) + " Thousand";
+        currentmoney.textContent = (przechowaj / 1e3).toFixed(2) + " Thousand";
     } else if (przechowaj >= 1e6 && przechowaj < 1e9) {
-        currentvalue.textContent = (przechowaj / 1e6).toFixed(2) + " Milion";
+        currentmoney.textContent = (przechowaj / 1e6).toFixed(2) + " Milion";
     } else if (przechowaj >= 1e9 && przechowaj < 1e12) {
-        currentvalue.textContent = (przechowaj / 1e9).toFixed(2) + " Bilion";
+        currentmoney.textContent = (przechowaj / 1e9).toFixed(2) + " Bilion";
     } else if (przechowaj >= 1e12 && przechowaj < 1e15) {
-        currentvalue.textContent = (przechowaj / 1e12).toFixed(2) + " Trilion";
+        currentmoney.textContent = (przechowaj / 1e12).toFixed(2) + " Trilion";
     } else if (przechowaj >= 1e15 && przechowaj < 1e18) {
-        currentvalue.textContent = (przechowaj / 1e15).toFixed(2) + " Quadrilion";
+        currentmoney.textContent = (przechowaj / 1e15).toFixed(2) + " Quadrilion";
     } else if (przechowaj >= 1e18 && przechowaj < 1e21) {
-        currentvalue.textContent = (przechowaj / 1e18).toFixed(2) + " Pentilion";
+        currentmoney.textContent = (przechowaj / 1e18).toFixed(2) + " Pentilion";
     } else if (przechowaj >= 1e21 && przechowaj < 1e24) {
-        currentvalue.textContent = (przechowaj / 1e21).toFixed(2) + " Sextilion";
+        currentmoney.textContent = (przechowaj / 1e21).toFixed(2) + " Sextilion";
     } else if (przechowaj >= 1e24 && przechowaj < 1e27) {
-        currentvalue.textContent = (przechowaj / 1e24).toFixed(2) + " Septilion";
+        currentmoney.textContent = (przechowaj / 1e24).toFixed(2) + " Septilion";
     } else if (przechowaj >= 1e27 && przechowaj < 1e30) {
-        currentvalue.textContent = (przechowaj / 1e27).toFixed(2) + " Octilion";
+        currentmoney.textContent = (przechowaj / 1e27).toFixed(2) + " Octilion";
     } else if (przechowaj >= 1e30 && przechowaj < 1e33) {
-        currentvalue.textContent = (przechowaj / 1e30).toFixed(2) + " Nonilion";
+        currentmoney.textContent = (przechowaj / 1e30).toFixed(2) + " Nonilion";
     } else if (przechowaj >= 1e33) {
-        currentvalue.textContent = (przechowaj / 1e33).toFixed(2) + " Decilion";
+        currentmoney.textContent = (przechowaj / 1e33).toFixed(2) + " Decilion";
     } else {
-        currentvalue.textContent = przechowaj;
+        currentmoney.textContent = przechowaj;
     }
     Bps();
     let wskazanie = document.querySelector('.wskaznik');
@@ -79,14 +80,14 @@ function perClick() {
         money -= clickCost;
         clickCost = clickValue * clickValue;
         wyswietl();
-        let wskazanie2 = document.querySelector('wskaznik2');
+        let wskazanie2 = document.querySelector('.wskaznik2');
         wskazanie2.textContent = "Wartość kliknięcia:" + clickValue + " Cena:" + clickCost;
     }
 }
 let divs = document.querySelectorAll('.pszczola')
 let divs2 = document.querySelectorAll('.upgrader')
 
-for (let i = 0; i < pszczoly.length; i++) {
+for (let i = 0; i < divs.length; i++) {
     let pszczola = pszczoly[i]
     divs[i].querySelector('button').addEventListener('click', () => {
         if (money >= pszczola.cena) {
@@ -94,9 +95,9 @@ for (let i = 0; i < pszczoly.length; i++) {
             money -= pszczola.cena;
             pszczola.cena = Math.round(pszczola.cena * 1.3)
             wyswietl();
-
-            let p = divs[i].querySelector('p');
-            currentvalue.textContent = "Ilość pszczół:" + pszczola.ile + " Cena pszczoły:" + pszczola.cena;
+            console.log(divs[i])
+            let cenapszczoly = divs[i].querySelector('.cenapszczoly');
+            cenapszczoly.textContent = "Ilość pszczół:" + pszczola.ile + " Cena pszczoły:" + pszczola.cena;
         }
     })
 
@@ -105,7 +106,7 @@ for (let i = 0; i < pszczoly.length; i++) {
         wyswietl()
     }, pszczola.coIle)
 }
-for (let i = 0; i < pszczoly.length; i++) {
+for (let i = 0; i < divs2.length; i++) {
     let upgrader = upgrade[i]
     const btn = divs2[i].querySelector('button')
     btn.addEventListener('click', () => {
@@ -136,6 +137,7 @@ function Bps() {
     for (let i = 0; i < pszczoly.length; i++) {
         Bps += 1000 / pszczoly[i].coIle * pszczoly[i].ile
     }
-    let wskazanie3 = document.querySelector('wskaznik3');
+    let wskazanie3 = document.querySelector('.wskaznik3');
     wskazanie3.textContent = "Bps: " + Bps;
 }
+
