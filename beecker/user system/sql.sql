00e9934cec51c2c987013bc60b2b9149f70d3439
Terrarian70 <46934544+Terrarian70@users.noreply.github.com>
2019-12-02 16:47:22 +0100

    Update sql.sql

--- beecker/user system/sql.sql
+++ beecker/user system/sql.sql
@@ -6,6 +6,5 @@ CREATE TABLE IF NOT EXISTS users (
   id int(11) NOT NULL AUTO_INCREMENT,
   login varchar(32) NOT NULL,
   pass varchar(32) NOT NULL,
-  email varchar(255) NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
