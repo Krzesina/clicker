f15334b9c8147f998329f672a67504271a73b549
Terrarian70 <46934544+Terrarian70@users.noreply.github.com>
2019-12-02 16:43:38 +0100

    Update register.php

--- beecker/user system/register.php
+++ beecker/user system/register.php
@@ -10,12 +10,9 @@ if (isset($_POST['send'])) {
     $login = mysqli_real_escape_string($conn, htmlspecialchars($_POST['login']));
     $pass = mysqli_real_escape_string($conn, htmlspecialchars($_POST['pass']));
     $pass_v = mysqli_real_escape_string($conn, htmlspecialchars($_POST['pass_v']));
-    $email = mysqli_real_escape_string($conn, htmlspecialchars($_POST['email']));
-    $email_v = mysqli_real_escape_string($conn, htmlspecialchars($_POST['email_v']));
-
+   
   
     $existsLogin = mysqli_fetch_array(mysqli_query($conn, "SELECT COUNT(*) FROM users WHERE login='$login' LIMIT 1"));
-    $existsEmail = mysqli_fetch_array(mysqli_query($conn, "SELECT COUNT(*) FROM users WHERE email='$email' LIMIT 1"));
 
     $errors = ''; 
 
@@ -23,8 +20,6 @@ if (isset($_POST['send'])) {
    
     if (!$login || !$email || !$pass || !$pass_v || !$email_v ) $errors .= '- Musisz wypełnić wszystkie pola<br />';
     if ($existsLogin[0] >= 1) $errors .= '- Ten login jest zajęty<br />';
-    if ($existsEmail[0] >= 1) $errors .= '- Ten e-mail jest już używany<br />';
-    if ($email != $email_v) $errors .= '- E-maile się nie zgadzają<br />';
     if ($pass != $pass_v)  $errors .= '- Hasła się nie zgadzają<br />';
 
    
@@ -37,7 +32,7 @@ if (isset($_POST['send'])) {
 
         $pass = user::passSalter($pass);
 
-        mysqli_query($conn, "INSERT INTO users (login, email, pass) VALUES('$login','$email','$pass');") or die ('<p class="error">Wystąpił błąd w zapytaniu i nie udało się zarejestrować użytkownika.</p>');
+        mysqli_query($conn, "INSERT INTO users (login, pass) VALUES('$login','$pass');") or die ('<p class="error">Wystąpił błąd w zapytaniu i nie udało się zarejestrować użytkownika.</p>');
 
         echo '<p class="success">'.$login.', zostałeś zarejestrowany.
         <br /><a href="login.php">Logowanie</a></p>';
@@ -54,13 +49,6 @@ if (isset($_POST['send'])) {
 
  <label for="pass_again">Hasło (ponownie):</label>
  <input maxlength="32" type="password" name="pass_v" id="pass_again" />
-
- <label for="email">Email:</label>
- <input type="text" name="email" maxlength="50" id="email" />
-
- <label for="email_again">Email (ponownie):</label>
- <input type="text" maxlength="255" name="email_v" id="email_again" /><br />
-
 
  <input type="hidden" name="send" value="1" />
  <input type="submit" value="Zarejestruj" />
